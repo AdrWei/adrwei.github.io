a4025bb0140c2b4f2621fcf24af9af610f0569c5
Adrian Wei <165309141@qq.com>
2025-03-14 16:28:44 +0800

    Delete callback_handler.js

--- callback_handler.js
+++
@@ -1,8 +0,0 @@
-// callback_handler.js
-const urlParams = new URLSearchParams(window.location.search);
-const code = urlParams.get('code');
-if (code) {
-  console.log('Received code:', code);
-  localStorage.setItem('authorization_code', code);
-  alert('Callback received! Code: ' + code);
-}
