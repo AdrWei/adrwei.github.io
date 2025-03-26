de87655adfec143a4b77d7670502046206deeadc
Adrian Wei <165309141@qq.com>
2025-03-26 17:07:46 +0800

    添加手机端导航加载流程.js

--- scripts/header-footer.js
+++ scripts/header-footer.js
@@ -3,6 +3,16 @@ fetch('/header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header').innerHTML = data;
+
+    // 在 header 加载完成后，添加切换导航栏的代码
+    const nav = document.querySelector('.nav');
+    const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+
+    if (mobileNavToggle) { // 确保 mobileNavToggle 存在，防止报错
+      mobileNavToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+      });
+    }
   });
 
 // 加载 footer
