a7d6205aa894ebc797df6e4524ce59b2965dfc9a
Adrian Wei <165309141@qq.com>
2025-03-26 17:24:14 +0800

    尝试修改，新增堆手机导航栏的应用.js

--- scripts/header-footer.js
+++ scripts/header-footer.js
@@ -1,23 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
 // 加载 header
 fetch('/header.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('header').innerHTML = data;
+.then(response => response.text())
+.then(data => {
+document.getElementById('header').innerHTML = data;
 
-    // 在 header 加载完成后，添加切换导航栏的代码
-    const nav = document.querySelector('.nav');
-    const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+// 在 header 加载完成后，添加切换导航栏的代码
+const nav = document.querySelector('nav');
+const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
 
-    if (mobileNavToggle) { // 确保 mobileNavToggle 存在，防止报错
-      mobileNavToggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
-      });
-    }
-  });
+if (mobileNavToggle) { // 确保 mobileNavToggle 存在，防止报错
+mobileNavToggle.addEventListener('click', () => {
+nav.classList.toggle('active');
+});
+}
+});
 
 // 加载 footer
 fetch('/footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('footer').innerHTML = data;
-  });
+.then(response => response.text())
+.then(data => {
+document.getElementById('footer').innerHTML = data;
+});
+});
