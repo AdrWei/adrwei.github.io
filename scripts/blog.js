f2fed3a2996c2e4050989efd35083e6c06bdc919
Adrian Wei <165309141@qq.com>
2025-04-07 14:57:00 +0800

    Update blog.js

--- scripts/blog.js
+++ scripts/blog.js
@@ -6,7 +6,7 @@ let allPosts = []; // 保存所有文章数据
 // 获取文章列表
 async function fetchPosts() {
     try {
-        const response = await fetch('/post/'); // 读取 /post 目录下的文件
+        const response = await fetch('/posts/'); // 读取 /post 目录下的文件
         const html = await response.text();
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
